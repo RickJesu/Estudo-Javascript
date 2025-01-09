84b0bb96e2aa0715945f2303f485cf1d492cc545
RickJesu <jesushenrique965@gmail.com>
2025-01-09 17:30:10 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,26 @@
+// Entrada de dados
+let nomeHeroi = "Capitã Marvel";
+const experiencia = 980.0;
+let nivel; 
+
+// Processamento (Usando If Else)
+if (experiencia <= 1000) {
+    nivel = "Ferro";
+} else if (experiencia >= 1001 && experiencia <= 2000) {
+    nivel = "Bronze";
+} else if (experiencia >= 2001 && experiencia <= 5000) {
+    nivel = "Prata";
+} else if (experiencia >= 5001 && experiencia <= 7000) {
+    nivel = "Ouro";
+} else if (experiencia >= 7001 && experiencia <= 8000) {
+    nivel = "Platina";
+} else if (experiencia >= 8001 && experiencia <= 9000) {
+    nivel = "Ascendente";
+} else if (experiencia >= 9001 && experiencia <= 10000) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
+}
+
+// Saída (print)
+console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`);
